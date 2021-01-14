7e18b87b578c06218d7959dd498a9a823183a5ca
Alon Bronshtein <allen.bronshtein@gmail.com>
2021-01-14 02:50:42 +0200

    started preprocessing

--- course.py
+++ course.py
@@ -1,4 +1,8 @@
 class Course:
-    def __init__(self,number):
-        self.number = number
+    def __init__(self,document):
+        self.number = document["_id"]
+        self.name = document["name"]
+        self.year = document["year"]
+        self.is_must = document["is_must"]
+        self.groups = document["groups"]
         # continue here
--- user.py
+++ user.py
@@ -1,7 +1,15 @@
+import course
 class User():
     def __init__(self,appData):
         self.appData = appData
     
     def run(self):
+        self.preprocess()
+           
+    def preprocess(self):
         self.appData.db_controller.pull(self.appData)
-        print(self.appData.local)
+        local = []
+        year = self.appData.user_args[0]
+        redo_courses = self.appData.user_args[1]
+        optional_courses = self.appData.user_args[2]
+        unavailable = self.appData.user_args[3]
